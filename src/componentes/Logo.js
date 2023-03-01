a117c80173b0cf6634c0a122c1ba72cdc6d5de02
Marc0Franc0 <marcofranco340@gmail.com>
2023-03-01 20:36:22 -0300

    update

---
+++ src/componentes/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Logo from '.././imagenes/logo.png';
+import '../hojas-de-estilo/Logo.css';
+
+
+const Logo = () => (
+
+  <div className='logo-contenedor'>
+  <img
+    src={Logo}
+    className='logo'
+    alt='Logo React' />
+</div>
+
+);
+export default Logo;
